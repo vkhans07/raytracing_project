1eb26f261b287d20d30a34359755b14a5f173645
Vijay Kumar Hans <vkhans@Vijays-MacBook-Pro.local>
2026-06-28 16:44:36 -0700

    getting ready for transition to viewports

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "color.h"
+#include "ray.h"
+#include "vec3.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
-    int image_height = 256;
-    int image_width = 256;
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
 
     // render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
