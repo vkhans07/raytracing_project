04bcef38e72ab451bca2260434ac8bd79c8dc5ff
Vijay Kumar Hans <vkhans@Vijays-MacBook-Pro.local>
2026-06-28 16:09:28 -0700

    vec 3 class done

---
+++ vec3.h
@@ -0,0 +1,96 @@
+//
+// Created by Vijay Kumar Hans on 6/28/26.
+//
+
+#ifndef GRAPHICS_PROJECT_VEC3_H
+#define GRAPHICS_PROJECT_VEC3_H
+
+class vec3 {
+public:
+    double e[3];
+
+    // constructors
+    vec3() : e{0,0,0} {};
+    vec3(double e0, double e1, double e2): e{e0, e1, e2} {};
+
+    // accessors
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    // self-operators
+    vec3 operator-() const { return vec3{-e[0], -e[1], -e[2]}; }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) const { return e[i]; }
+
+    // binary operators
+     vec3 operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+
+        return *this;
+    }
+
+    vec3 operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+
+        return *this;
+    }
+
+    vec3 operator /=(double t) {
+        return *this *= 1/t;
+    }
+
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+};
+
+using point3 = vec3;
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& v, const vec& u) {
+    return vec3(v.e[0] + u.e[0], v.e[1] + u.e[1], v.e[2] + u.e[2]);
+}
+
+inline vec3 operator-(const vec3& v, const vec& u) {
+    return vec3(v.e[0] - u.e[0], v.e[1] - u.e[1], v.e[2] - u.e[2]);
+}
+
+inline vec3 operator*(double x, const vec3& v) {
+    return vec3(x * v.e[0], x * v.e[1], v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double x) {
+    return x * v;
+}
+
+inline vec3 operator/(const vec3&v, double x) {
+    return v * (1/x);
+}
+
+inline double dot(const vec3& v, const vec3& u) {
+    return v.e[0] * u.e[0] + v.e[1] * u.e[1] + v.e[2] * u.e[2];
+}
+
+inline vec3 cross(const vec3& v, const vec3& u) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v) {
+    return v / v.length();
+}
+
+#endif //GRAPHICS_PROJECT_VEC3_H
