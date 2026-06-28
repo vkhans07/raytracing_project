ba43611a33a4bc96082983d479ff8da65186cfd2
Vijay Kumar Hans <vkhans@Vijays-MacBook-Pro.local>
2026-06-28 15:13:31 -0700

    added logging for scanlines left

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@ int main() {
     // render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int i = 0; i < image_height; i++) {
-        for (int j = 0; j < image_width; j++) {
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.0;
@@ -22,4 +23,7 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone.                        \n" << std::flush;
+
+    return 0;
 }
