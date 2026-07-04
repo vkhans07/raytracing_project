17f8a760a374194b80f40891ee97e98187c0b550
vkhans07 <vkhans@berkeley.edu>
2026-07-04 14:19:52 -0700

    implemented first sphere collision

--- main.cpp
+++ main.cpp
@@ -3,8 +3,19 @@
 #include "ray.h"
 #include "vec3.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    return (b * b - 4 * a * c) >= 0;
+}
 
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
