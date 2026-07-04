f3e82da1c8a4cc6474c22372a8f5b6195fc29f30
vkhans07 <vkhans@berkeley.edu>
2026-07-04 14:33:06 -0700

    changed hit sphere to give a normal vector

--- main.cpp
+++ main.cpp
@@ -2,20 +2,26 @@
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+
+// returns value of t, enforcing positive
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    return (b * b - 4 * a * c) >= 0;
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto disriminant = h*h - a * c;
+    if (disriminant < 0) {
+        return -1.0;
+    }
+    return (h - std::sqrt(disriminant)) / a;
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
-
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
