bb1148c658066717f9cced0afde12b3471b1a00c
vkhans07 <vkhans@berkeley.edu>
2026-07-04 16:46:09 -0700

    diffuse objects, eliminated shadow acne

--- camera.h
+++ camera.h
@@ -12,6 +12,7 @@ public:
     double aspect_ratio = 1.0;
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -23,7 +24,7 @@ public:
                 color pixel_color(0,0,0);
                 for (int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -63,10 +64,15 @@ private:
         pixel00_loc = viewport_upper_left + pixel_delta_u / 2 + pixel_delta_v / 2;
     }
 
-    color ray_color(const ray& r, const hittable& world) {
+    color ray_color(const ray& r, int depth, const hittable& world) {
+        if (depth <= 0) {
+            return color(0,0,0);
+        }
+
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            vec3 direction = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
